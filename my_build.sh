95cf6ded6e5a5c20f1c5225585fb817a3df48169
fhtuft <fhtuft@ifi.uio.no>
2020-01-10 20:48:49 +0100

    removed some options

--- my_build.sh
+++ my_build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-./configure   --prefix="$HOME/ffmpeg_build"   --pkg-config-flags="--static"   --extra-cflags="-I$HOME/ffmpeg_build/include"   --extra-ldflags="-L$HOME/ffmpeg_build/lib"   --extra-libs="-lpthread -lm"   --bindir="$HOME/bin"   --enable-gpl   --enable-libass   --enable-libfdk-aac   --enable-libfreetype   --enable-libmp3lame   --enable-libopus   --enable-libvorbis   --enable-libvpx   --enable-libx264   --enable-libx265   --enable-nonfree && PATH="$HOME/bin:$PATH" make && make install && hash -r
 
+
+./configure   --prefix="$HOME/ffmpeg_build"   --pkg-config-flags="--static"   --extra-cflags="-I$HOME/ffmpeg_build/include"   --extra-ldflags="-L$HOME/ffmpeg_build/lib"   --extra-libs="-lpthread -lm"   --bindir="$HOME/bin"   --enable-gpl   --enable-libass   --enable-libfdk-aac   --enable-libfreetype   --enable-libmp3lame  --enable-libx264  --enable-nonfree && PATH="$HOME/bin:$PATH" make -j 8 && make install && hash -r
+
+
+# --enable-libx265 not include as there is a bug in my current ubuntu package preventing builing with ffmpeg
